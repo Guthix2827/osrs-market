be168a39e693b85827c0bde9d0eedfb9a3cbacf3
Leonid Beilis <leonid.beilis@gmail.com>
2026-08-19 02:25:06 +0300

    user agent updated for IconDownloader.cpp

--- backend/src/icons/IconDownloader.cpp
+++ backend/src/icons/IconDownloader.cpp
@@ -13,7 +13,7 @@ constexpr auto WIKI_IMAGE_BASE_URL =
     "https://oldschool.runescape.wiki/images/";
 
 constexpr auto USER_AGENT =
-    "osrs-market-alpha/0.1";
+    "osrs-market/0.1 (github.com/Guthix2827/osrs-market)";
 }
 
 IconDownloader::IconDownloader(
